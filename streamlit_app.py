08c8cc004d09790f4a5240b143d89cb3a4124068
sankha90 <64725626+sankha90@users.noreply.github.com>
2021-06-24 16:03:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ if add_selectbox == "Read Something":
 # For a reference to a collection, we use .stream() instead of .get()
     for doc in blg_ref.stream():
         t2 = doc.to_dict()
-        st.markdown(t2['**Name**'])
+        st.write(t2['Name'])
         with st.beta_expander("Expand to see what's beyond 🕵️‍♂️"):
             st.write(t2['Analysis'])
         st.write(t2['Emp_ID'])
