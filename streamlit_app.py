eaae8d89c9ae6e1c469221fb8df6d0375956a8ac
sankha90 <64725626+sankha90@users.noreply.github.com>
2021-06-24 15:54:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from google.cloud import firestore
 import json
+from annotated_text import annotated_text
+
 
 #key_dict = json.loads(st.secrets["textkey"])
 #creds = service_account.Credentials.from_service_account_info(key_dict)
